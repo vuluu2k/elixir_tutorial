ecd4a58043b9c2c47da901540e904aa45e73e0ea
vuluu <tieungpk1@gmail.com>
2022-03-13 21:08:28 +0700

    create elixir

---
+++ lib/test1.ex
@@ -0,0 +1,18 @@
+defmodule Test1 do
+  @moduledoc """
+  Documentation for `Test1`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Test1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
